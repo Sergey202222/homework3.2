00acbf3122aca2ab252c6a6d4e3effbe42b1b0c7
Sergey <57405954+Sergey202222@users.noreply.github.com>
2025-03-04 23:08:44 +0300

    Add files via upload

---
+++ homework2/homework2.2.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+class Counter
+{
+public:
+    Counter();
+    Counter(int);
+    void add();
+    void subtract();
+    int equals();
+private:
+    int counter;
+};
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string answer{};
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+    std::cin >> answer;
+    int startCounter{};
+    if (answer == "да")
+    {
+        std::cout << "Введите начальное значение счётчика: ";
+        std::cin >> startCounter;
+    }
+
+    Counter counter = (answer == "да")?Counter() : Counter(startCounter);
+    char simbol{};
+    while (simbol != 'x')
+    {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> simbol;
+
+        switch (simbol)
+        {
+        case '+':
+            counter.add();
+            break;
+        case '-':
+            counter.subtract();
+            break;
+        case '=':
+            std::cout << counter.equals() << '\n';
+            break;
+        case 'x':
+            std::cout << "До свидания!";
+            break;
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+Counter::Counter()
+{
+    counter = 1;
+}
+
+Counter::Counter(int counter)
+{
+    this->counter = counter;
+}
+
+void Counter::add()
+{
+    ++counter;
+}
+
+void Counter::subtract()
+{
+    --counter;
+}
+
+int Counter::equals()
+{
+    return counter;
+}
